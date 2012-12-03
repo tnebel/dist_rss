da8d25c32ef3d0221628a15de72a8180c349d30b
Paul Schultz <pwschult@unix11.andrew.cmu.edu>
2012-12-02 19:31:32 -0500

    Added capacity for multiple storage servers, but haven't tested.

--- go/src/rssstoreserver/rssstoreserver.go
+++ go/src/rssstoreserver/rssstoreserver.go
@@ -13,14 +13,17 @@ import (
 func main() {
     var mn string
     var port int
+    var numNodes int
 
-    flag.StringVar(&mn, "m", "localhost:5001", "hostport of master node")
+    flag.StringVar(&mn, "m", "localhost:5002", "hostport of master storage node")
     flag.IntVar(&port, "p", 5002, "Listening port of rssstore")
+    flag.IntVar(&numNodes, "N", 0, "Become the master.  Specifies the number of nodes in the system, including the master.")
+
     flag.Parse()
 
     //p := uint32(port)
-    
-    rs, err := rssstore.NewRssStore(mn, port, 1)
+
+    rs, err := rssstore.NewRssStore(mn, port, 1, numNodes)
 
     if err != nil {
         fmt.Printf("Error in creating rss store")
